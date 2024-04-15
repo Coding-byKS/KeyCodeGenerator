62044e4bd7b1c19906e9fb3c603b27b3e0b679ea
Coding-byKS <khushboo.singh43@gmail.com>
2024-04-15 13:51:07 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// alert("hi")
+document.addEventListener("keydown", (event) =>{
+    let key =event.key;
+    let code=event.code;
+    let which=event.which;
+    document.getElementById("detail").innerHTML = "You pressed " + key +"!!";
+    document.getElementById("itemkey").innerHTML = "event.key";
+    document.getElementById("itemcode").innerHTML = "event.code";
+    document.getElementById("itemwhich").innerHTML = "event.which";
+    document.getElementById("key").innerHTML = key;
+    document.getElementById("code").innerHTML = code;
+    document.getElementById("which").innerHTML = which;
+})
+
